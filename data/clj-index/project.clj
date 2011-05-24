ed364651cb5d788a9e3a2f27963cb7a67b269772
Richard Drake <rdrake@NCR.oncampus.local>
2011-05-24 15:27:33 -0400

    Added Clojure index code.

---
+++ data/clj-index/project.clj
@@ -0,0 +1,6 @@
+(defproject index "1.0.0-SNAPSHOT"
+            :description      "Clojure T2 indexer."
+            :dependencies     [[org.clojure/clojure         "1.2.1"]
+                               [org.clojure/clojure-contrib "1.2.0"]]
+            :dev-dependencies [[clojureql                   "1.0.0"]
+                               [postgresql/postgresql       "9.0-801.jdbc4"]])
---
+++ data/clj-index/test/index/test/core.clj
@@ -0,0 +1,6 @@
+(ns index.test.core
+  (:use [index.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
