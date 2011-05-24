d692fae86abc44b4ca4b9a99c36cbca6d12eefc4
Richard Drake <rdrake@rdrake.org>
2011-05-24 16:50:48 -0400

    Forgot to include some updates.

--- data/clj-index/project.clj
+++ data/clj-index/project.clj
@@ -1,6 +1,8 @@
 (defproject index "1.0.0-SNAPSHOT"
             :description      "Clojure T2 indexer."
-            :dependencies     [[org.clojure/clojure         "1.2.1"]
-                               [org.clojure/clojure-contrib "1.2.0"]]
-            :dev-dependencies [[clojureql                   "1.0.0"]
-                               [postgresql/postgresql       "9.0-801.jdbc4"]])
+            :dependencies     [[org.clojure/clojure               "1.2.1"]
+                               [org.clojure/clojure-contrib       "1.2.0"]
+                               [clojureql                         "1.0.0"]
+                               [org.xerial/sqlite-jdbc            "3.7.2"]
+                               [org.apache.lucene/lucene-core     "3.0.3"]
+                               [org.apache.lucene/lucene-queries  "3.0.3"]])
